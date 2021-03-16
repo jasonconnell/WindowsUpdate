d4488ca9a60c81ee09185051965b26b4c57e9043
jasonconnell <connell.jason.d@gmail.com>
2021-03-15 23:02:49 -0500

    new revision

--- WindowsUpdate.ps1
+++ WindowsUpdate.ps1
@@ -104,10 +104,184 @@ Function Get-DiskSpace{
 
 [CmdletBinding(SupportsShouldProcess=$True, ConfirmImpact='Low')]
 Param ()
+
+    Begin{
+        $SystemDrive = $ENV:SystemDrive
+
+    }#End Begin
+
+    Process{
+        Write-Debug "Getting System Drive $($SystemDrive) information"
+        $DriveInfo = get-WmiObject win32_logicaldisk -Filter "DeviceID='$($SystemDrive)'"
+    }#End Process
+
+    End{
+        $FreeSpace = [Math]::Round($DriveInfo.FreeSpace / 1Gb)
+        $DiskSpacePercent = [Math]::Round(($DriveInfo.FreeSPace / $DriveInfo.Size) * 100)
+        Write-Verbose "System drive currently has $($FreeSpace) GB free ($($DiskSpacePercent)%)"     
+        return $FreeSpace 
+    }#End End
+}#End Function Get-DiskSpace
+
+
+Function Get-SystemHashTable{
+<#
+.SYNOPSIS
+    This function will build an object to store key information.
+
+.NOTES
+    Version:        1.0
+    Author:         Jason Connell
+    Creation Date:  3/15/2021
+    Purpose/Change: Initial script development
+
+.LINK
+    https://github.com/jasonconnell/WindowsUpdate/blob/main/README.md
+#>
+
+[CmdletBinding(SupportsShouldProcess=$True, ConfirmImpact='Low')]
+Param ()
+
+
+    Begin{
+        Write-Debug "Creating PS hash table to store system information"
+    }#End Begin
+
+    Process{
+
+        Write-Debug "Getting system version information"
+        Try{
+            $SystemVerInfo = [System.Environment]::OSVersion.Version
+            $MajorVersion = $SystemVerInfo.Major
+            $MinorVersion = $SystemVerInfo.Minor
+            $BuildNumber = $SystemVerInfo.Build     
+        }#End Try
+        
+        Catch{
+            Write-Error "ERROR: Line ($($LINENUM): Failed to retrieve version information"
+        }#End Catch
+
+        Try{
+            Write-Debug "Getting System Name and OS"
+            $ComputerName = [System.Net.DNS]::GetHostName()
+            $OperatingSystem = (Get-WmiObject Win32_OperatingSystem).Caption
+        }#End Try
+
+        Catch{
+            Write-Error "ERROR: Line $($LINENUM): Failed to gather System name and OS"
+        }#End Catch
+
+    }#End Process
+
+    End{
+        $SystemHash = @{
+            ComputerName = $ComputerName
+            OperatingSystem = $OperatingSystem
+            BuildNumber = $BuildNumber
+            MajorVersion = $MajorVersion
+            MinorVersion = $MinorVersion
+            Revison = $Revison
+
+        }
+        return $SystemHash
+    }#End End
+}#End Function Get-SystemHashTable
+
+
+Function Backup-UserProfile{
+<#
+.SYNOPSIS
+    This function will create a backup of all users profiles and settings.
+
+.NOTES
+    Version:        1.0
+    Author:         Jason Connell
+    Creation Date:  3/15/2021
+    Purpose/Change: Initial script development
+#>
+
+[CmdletBinding(SupportsShouldProcess=$True, ConfirmImpact='Low')]
+Param ()
+
+    Begin{}
+
+    Process{}
+
+    End{}
 }
 
 
+Function Get-DownloadSpeed{
+<#
+.SYNOPSIS
+    This function will preform a quick test to determine the systems download speed.
 
+.NOTES
+    Version:        1.0
+    Author:         Jason Connell
+    Creation Date:  3/15/2021
+    Purpose/Change: Initial script development
+#>
+
+[CmdletBinding(SupportsShouldProcess=$True, ConfirmImpact='Low')]
+Param ()    
+}
+
+Function Update-Windows10 {
+    <#
+.SYNOPSIS
+    This function will update a Windows 10 system to the latest build.
+.NOTES
+    Version:        1.0
+    Author:         Jason Connell
+    Creation Date:  3/15/2021
+    Purpose/Change: Initial script development
+#>
+
+[CmdletBinding(SupportsShouldProcess=$True, ConfirmImpact='Low')]
+Param (
+    [string]$UpdateTool = 'https://download.microsoft.com/download/2/b/b/2bba292a-21c3-42a6-8123-98265faff0b6/Windows10Upgrade9252.exe',
+    [switch]$BackupUserProfile,
+    [int]$FreeSpaceThreshold = 20
+)
+
+    Begin{
+        if ((Get-DiskSpace) -ge $FreeSpaceThreshold){
+            Write-Verbose "System disk check passed. Continuing with update"
+        }Else{
+            Write-Error "ERROR: Line $($LINENUM): Not enough free disk space to continue."
+            return
+        }
+
+        if ($BackupUserProfile){
+            Write-Verbose "User Profile Backup switch set to true. Starting backup of profile"
+            Backup-UserProfile
+        }Else{
+            Write-Verbose "User Profile Backup switch set to false. Skipping backup of profile"
+        }
+    }
+
+    Process{
+        Try{
+            Write-Verbose "Creating temp directory at $($ENV:TEMP)\Windows10Update"
+            New-Item -Path $ENV:TEMP -Name "Windows10Update" -ItemType Directory -Force -ErrorAction Stop | Out-Null
+        }
+
+        Catch{
+            Write-Error "Failed to create temp directory to store update data" -ErrorAction Stop
+        }
+
+        Try{
+            (New-Object Net.Webclient).DownloadFile($UpdateTool,($ENV:TEMP + "\Windows10Update\updater.exe"))
+        }
+
+        Catch{
+            Write-Error "ERROR: Line ($($LINENUM): Failed to download file $($UpdateTool) $($Error[0])" -ErrorAction Stop
+        }
+    }
+
+    End{}
+}
 
 Function Get-CurrentLineNumber {
     $MyInvocation.ScriptLineNumber
